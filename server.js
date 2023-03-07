a4da203febcbbce5ad2f2a87954d51fa192e74c9
Wrecker1023 <wrecker1023@gmail.com>
2023-03-06 19:18:35 -0500

    added middleware and ability to post new animal

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express();
+//add middleware:
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming json data
+app.use(express.json());
 
 function filterByQuery( query, animalsArray) {
     let personalityTraitsArray = [];
@@ -47,6 +52,13 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    return animal;
+}
+
 app.get('/api/animals', (req,res) => {
     let results = animals;
     if(req.query) {
@@ -64,7 +76,20 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+// set up a route on our server that
+// accepts data to be used or stored server-side
+app.post('/api/animals', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //add animal to json file and animals arr in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
 
+//added ability to post new animal and added middleware
