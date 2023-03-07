a9449a5b41302384349879d67fe6b5f8cfdb2753
Wrecker1023 <wrecker1023@gmail.com>
2023-03-06 19:26:31 -0500

    added validation function for animal array

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
 //add middleware:
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -55,8 +58,27 @@ function findById(id, animalsArray) {
 function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
-
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
     return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 app.get('/api/animals', (req,res) => {
@@ -82,10 +104,14 @@ app.post('/api/animals', (req, res) => {
     //set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    //add animal to json file and animals arr in this function
-    const animal = createNewAnimal(req.body, animals);
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not formatted properly');
+    } else {
+        //add animal to json file and animals arr in this function
+        const animal = createNewAnimal(req.body, animals);
 
-    res.json(animal);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
