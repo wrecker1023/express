0d3996586e12d16e9307167e8c540b357fdf18ac
Wrecker1023 <wrecker1023@gmail.com>
2023-03-14 18:55:11 -0400

    added zookeepers json and querys

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -1,4 +1,3 @@
-
 const router = require("express").Router();
 const { filterByQuery, findById, createNewAnimal, validateAnimal, } = require("../../lib/animals");
 const { animals } = require("../../data/animals");
